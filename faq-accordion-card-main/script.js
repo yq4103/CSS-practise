ca776fd1839aa18370116b39cdc1e05a2a85fe9b
Yirong Qin <yq4103@mun.ca>
2022-01-03 23:13:10 -0500

    accordion with dropdown

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,1 +1,6 @@
-console.log("TEST");
+document.querySelectorAll('.accordion__question').forEach(item => {
+  item.addEventListener('click', event => {
+    item.classList.toggle('open');
+    item.nextElementSibling.classList.toggle('open');
+  });
+});
