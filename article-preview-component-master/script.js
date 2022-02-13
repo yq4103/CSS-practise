afcf175ddb9c92c3d4ce28baf750b5884032ea5d
Yirong Qin <yq4103@mun.ca>
2022-02-12 20:19:30 -0500

    mobile version complete

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,6 @@
+document.getElementById("button").addEventListener("click", ()=>{
+  document.getElementById("grey").style.display = "flex";
+  document.getElementById("white").style.display = "none";
+  document.getElementById("buttonClose").style.display = "inline-block";
+  document.getElementById("button").style.display = "none";
+});
